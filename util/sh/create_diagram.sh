aad9d8cb696272892acf41ecb3ddd3febfae2b25
Nobuchika Tanaka <codequokka@gmail.com>
2022-02-19 10:59:55 +0900

    Refactor InitialStack

---
+++ util/sh/create_diagram.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cfn_templates_dir='./cfn/templates'
+drawio_diagram_dir='./docs/drawio'
+
+for cfn_template_file in $(ls $cfn_templates_dir)
+do
+  echo $cfn_template_file
+  ./node_modules/.bin/cfn-dia draw.io -c -t ${cfn_templates_dir}/${cfn_template_file} -o ${drawio_diagram_dir}/${cfn_template_file}.drawio
+done
